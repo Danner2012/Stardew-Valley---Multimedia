2c9340173780e5039daa34ed1bfe243903c506dd
Danner2012 <dannerpantoja@gmail.com>
2025-09-25 11:54:41 -0400

    Add files via upload
    pruebas de buffs en el juego

---
+++ EfectosBolivianos/EfectosBolivianos/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace EfectosBolivianos
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            // Evento para detectar cuando se presiona una tecla
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // Ignorar si el jugador aún no cargó la partida
+            if (!Context.IsWorldReady)
+                return;
+
+            Farmer player = Game1.player;
+
+            // Detectar si se presiona la tecla V
+            if (e.Button == SButton.V)
+            {
+                // Aplicar buff temporal
+                player.addedSpeed = 3f;   // +3 velocidad
+                player.stamina += 50f;    // +50 energía
+                Game1.addHUDMessage(new HUDMessage("Aumento de stamina", HUDMessage.achievement_type));
+            }
+        }
+    }
+}
